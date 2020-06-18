942158c60ba782918055de99f7892e94ed20f24a
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-06-18 03:03:55 -0400

    added handlebars functionality, html displaying, articles populate, need to work on sorting articles correctly

--- controllers/api.js
+++ controllers/api.js
@@ -9,6 +9,7 @@ var app = express();
 // Require all models
 var db = require("../models");
 
+
 //get route for npr website
 router.get("/scrape", function (req, res) {
     axios.get("https://www.npr.org/sections/news/").then(function (response) {
@@ -16,11 +17,12 @@ router.get("/scrape", function (req, res) {
         var $ = cheerio.load(response.data);
         // console.log(req);
         // console.log(res);
-        $(".title").each(function (i, element) {
+        $(".item-info").each(function (i, element) {
             var count = i;
             var result = {};
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
+            result.title = $(this).children(".title").children("a").text();
+            result.link = $(this).children(".title").children("a").attr("href");
+            result.teaser = $(this).children(".teaser").text();
 
             db.Article.create(result).then(function (dbArticle) {
                 console.log(dbArticle);
@@ -34,4 +36,33 @@ router.get("/scrape", function (req, res) {
     })
 });
 
+router.get("/", (req, res) => {
+    db.Article.find({}, null, { sort: { title: -1 } }).lean()
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            const retrievedArticles = dbArticle;
+            let hbsObject;
+            hbsObject = {
+                articles: dbArticle
+            };
+            res.render("index", hbsObject);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+// Route for getting all Articles from the db
+router.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 module.exports = router;
--- models/Article.js
+++ models/Article.js
@@ -15,6 +15,16 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    teaser: {
+        type: String,
+        default: false,
+        required: false
+    },
+    isSaved: {
+        type: Boolean,
+        default: false,
+        required: false
+    },
     // `note` is an object that stores a Note id
     // The ref property links the ObjectId to the Note model
     // This allows us to populate the Article with an associated Note
