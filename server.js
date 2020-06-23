4a07afc26bb5b496706cd66826683af8c3ae5785
Ken Dacanay <ken.p.r.dacanay@gmail.com>
2020-06-23 13:47:18 -0400

    check heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
+var cheerio = require("cheerio");
 
 
 // Require all models
@@ -13,6 +13,9 @@ var PORT = process.env.PORT || process.argv[2] || 8080;
 // Initialize Express
 var app = express();
 
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Configure middleware
 // Handlebars
 const exphbs = require("express-handlebars");
@@ -21,22 +24,25 @@ app.set("view engine", "handlebars");
 
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-
-// Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect("mongodb://localhost/NPR", { useNewUrlParser: true });
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
 // Controllers
 const router = require("./controllers/api");
 app.use(router);
 mongoose.set('useFindAndModify', false);
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect("mongodb://localhost/NPR", { useNewUrlParser: true });
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Make public a static folder
+app.use(express.static("public"));
+
+
 // Start the server
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
